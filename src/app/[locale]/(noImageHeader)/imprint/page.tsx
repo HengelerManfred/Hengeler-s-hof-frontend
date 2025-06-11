efad1d5dc83f586513fc89c076353bf93adaf5f8
EKorchevsky <korchevskyeduard@gmail.com>
2025-06-11 19:01:42 +0200

    PrivacyPilice and Imprint

---
+++ src/app/[locale]/(noImageHeader)/imprint/page.tsx
@@ -0,0 +1,29 @@
+export default function Imprint() {
+  return (
+    <main className="w-3/4 h-[63.8vh] mx-auto">
+      <h2 className="text-4xl font-bold my-6">Impressum</h2>
+      <section>
+        <ol className="list-decimal pl-6 marker:text-2xl marker:font-semibold">
+          <li>
+            <h3 className="text-2xl font-semibold">Angaben gemäß § 5 DDG</h3>
+            <p className="text-lg font-inter">[Name Familiename]</p>
+            <p>[Straße Nr.]</p>
+            <p>[PLZ Ort]</p>
+            <p>Deutschland</p>
+            <p className="mt-2">E-Mail: [E-Mail]</p>
+            <p>Telefon: [Telefon]</p>
+          </li>
+          <li>
+            <h3 className="text-2xl font-semibold">
+              Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV
+            </h3>
+            <p className="text-lg font-inter">[Name Familiename]</p>
+            <p>[Straße Nr.]</p>
+            <p>[PLZ Ort]</p>
+            <p>Deutschland</p>
+          </li>
+        </ol>
+      </section>
+    </main>
+  );
+}

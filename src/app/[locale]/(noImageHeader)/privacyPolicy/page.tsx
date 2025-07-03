a328885d587f1cf0d4122fa08ef8f913e2c3b8a9
MaxZhylka <132847452+MaxZhylka@users.noreply.github.com>
2025-07-03 22:36:39 +0300

    Adjust privacy policy (#21)
    * Privacy Policy Changes
    
    * add localization
    
    ---------
    
    Co-authored-by: EKorchevsky <korchevskyeduard@gmail.com>

--- src/app/[locale]/(noImageHeader)/privacyPolicy/page.tsx
+++ src/app/[locale]/(noImageHeader)/privacyPolicy/page.tsx
@@ -14,105 +14,74 @@ export default async function PrivacyPolicy() {
   const contacts = await loadContacts();
 
   return (
-    <main className="w-3/4 inter mx-auto py-10">
-      <h2 className="text-4xl font-bold">{t("title")}</h2>
-      <section>
-        <ol className="list-decimal pl-6 space-y-6 marker:text-2xl marker:font-semibold">
-          <li>
-            <h3 className="text-2xl font-semibold">{t("responsible.title")}</h3>
-            <p>{t("responsible.operator")}</p>
-            <p>{contacts.street}, {contacts.postalCode} {contacts.city}, {contacts.country}</p>
-            <p>{contacts.email}</p>
-            <p>{contacts.phoneNumber}</p>
-          </li>
-          <li>
-            <h3 className="text-2xl font-semibold">
-              {t("dataProcessing.title")}
-            </h3>
-            <h4 className="text-xl font-medium">
-              {t("dataProcessing.registration.title")}
-            </h4>
-            <p>{t("dataProcessing.registration.text")}</p>
-            <ul className="list-disc pl-6 space-y-1">
-              {t
-                .raw("dataProcessing.registration.items")
-                .map((item: string, index: number) => (
-                  <li key={index}>{item}</li>
-                ))}
-            </ul>
-            <p>{t("dataProcessing.registration.legal")}</p>
+    <main className="w-3/4 mx-auto">
+      <h2 className="text-3xl font-bold mb-4">{t("title")}</h2>
 
-            <h4 className="text-xl font-medium">
-              {t("dataProcessing.oauth.title")}
-            </h4>
-            <p>{t("dataProcessing.oauth.text")}</p>
-            <ul className="list-disc pl-6 space-y-1">
-              {t
-                .raw("dataProcessing.oauth.items")
-                .map((item: string, index: number) => (
-                  <li key={index}>{item}</li>
-                ))}
-            </ul>
-            <p>{t("dataProcessing.oauth.additional")}</p>
-            <p>{t("dataProcessing.oauth.legal")}</p>
+      <h3 className="text-2xl font-semibold mt-6 mb-2">
+        {t("responsibleTitle")}
+      </h3>
+      <span className="mb-4">
+        {t("responsibleText")}
+        <br />
+        Manfred Hengeler
+        <br />
+        <p>
+          {t("responsibleAddress", {
+            street: contacts.street,
+            postalCode: contacts.postalCode,
+            city: contacts.city,
+            country: contacts.country,
+          })}
+        </p>
+        <p>{ contacts.email }</p>
+        <p> { contacts.phoneNumber }</p>
+      </span>
+      <p className="mb-4">{t("responsibleLaw")}</p>
 
-            <h4 className="text-xl font-medium">
-              {t("dataProcessing.visitData.title")}
-            </h4>
-            <p>{t("dataProcessing.visitData.text")}</p>
-            <p>{t("dataProcessing.visitData.legal")}</p>
+      <h3 className="text-2xl font-semibold mt-6 mb-2">{t("dataTitle")}</h3>
+      <p className="mb-4">{t("dataText")}</p>
+      <h4 className="text-xl font-semibold mt-4 mb-2">{t("stripeTitle")}</h4>
+      <ul className="list-disc list-inside mb-4">
+        {t.raw("stripeList").map((item: string, idx: number) => (
+          <li key={idx}>{item}</li>
+        ))}
+      </ul>
+      <p className="mb-4">{t("stripeText")}</p>
 
-            <h4 className="text-xl font-medium">
-              {t("dataProcessing.payment.title")}
-            </h4>
-            <p>{t("dataProcessing.payment.text")}</p>
-            <p>{t("dataProcessing.payment.legal")}</p>
-          </li>
-          <li>
-            <h3 className="text-2xl font-semibold">{t("cookies.title")}</h3>
-            <p>{t("cookies.text")}</p>
-            <ul className="list-disc pl-6 space-y-1">
-              {t.raw("cookies.items").map((item: string, index: number) => (
-                <li key={index}>{item}</li>
-              ))}
-            </ul>
-            <p>{t("cookies.noTracking")}</p>
-            <p>{t("cookies.legal")}</p>
-          </li>
-          <li>
-            <h3 className="text-2xl font-semibold">{t("thirdParty.title")}</h3>
-            <ul className="list-disc pl-6 space-y-1">
-              {t.raw("thirdParty.items").map((item: string, index: number) => (
-                <li key={index}>{item}</li>
-              ))}
-            </ul>
-          </li>
-          <li>
-            <h3 className="text-2xl font-semibold">{t("retention.title")}</h3>
-            <ul className="list-disc pl-6">
-              {t.raw("retention.items").map((item: string, index: number) => (
-                <li key={index}>{item}</li>
-              ))}
-            </ul>
-          </li>
-          <li>
-            <h3 className="text-2xl font-semibold">{t("rights.title")}</h3>
-            <p>{t("rights.text")}</p>
-            <ul className="list-disc pl-6">
-              {t.raw("rights.items").map((item: string, index: number) => (
-                <li key={index}>{item}</li>
-              ))}
-            </ul>
-            <p>{t("rights.contact")}</p>
-            <p>{t("responsible.email")}</p>
-          </li>
-          <li>
-            <h3 className="text-2xl font-semibold">{t("updates.title")}</h3>
-            <p>{t("updates.validFrom")}</p>
-            <p>{t("updates.changes")}</p>
-          </li>
-        </ol>
-      </section>
+      <h4 className="text-xl font-semibold mt-4 mb-2">
+        {t("ownCookiesTitle")}
+      </h4>
+      <ul className="list-disc list-inside mb-4">
+        {t.raw("ownCookiesList").map((item: string, idx: number) => (
+          <li key={idx}>{item}</li>
+        ))}
+      </ul>
+      <p className="mb-4">{t("ownCookiesText")}</p>
+
+      <h3 className="text-2xl font-semibold mt-6 mb-2">{t("paymentTitle")}</h3>
+      <p className="mb-4">{t("paymentText1")}</p>
+      <p className="mb-4">{t("paymentText2")}</p>
+
+      <h3 className="text-2xl font-semibold mt-6 mb-2">{t("oauthTitle")}</h3>
+      <p className="mb-4">{t("oauthText1")}</p>
+      <p className="mb-4">{t("oauthText2")}</p>
+
+      <h3 className="text-2xl font-semibold mt-6 mb-2">{t("rightsTitle")}</h3>
+      <p className="mb-2">{t("rightsTitle")}</p>
+      <ul className="list-disc list-inside mb-4">
+        {t.raw("rightsList").map((item: string, idx: number) => (
+          <li key={idx}>{item}</li>
+        ))}
+      </ul>
+      <p className="mb-4">{t("rightsText")}</p>
+
+      <h3 className="text-2xl font-semibold mt-6 mb-2">
+        {t("retentionTitle")}
+      </h3>
+      <p className="mb-4">{t("retentionText")}</p>
+
+      <h3 className="text-2xl font-semibold mt-6 mb-2">{t("updateTitle")}</h3>
+      <p className="mb-4">{t("updateText")}</p>
     </main>
   );
 }

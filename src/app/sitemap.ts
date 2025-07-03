ac9bb6db5f278f77a68f8ab4326e58513c16d9a1
MaxZhylka <dexhonesta@gmail.com>
2025-07-03 23:45:01 +0300

    add sitemap and robot.txt

---
+++ src/app/sitemap.ts
@@ -0,0 +1,150 @@
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://www.hengelershof.com/en/',
+      lastModified: new Date(),
+      priority: 1.0,
+      changeFrequency: 'weekly',
+    },
+    {
+      url: 'https://www.hengelershof.com/uk/',
+      lastModified: new Date(),
+      priority: 1.0,
+      changeFrequency: 'weekly',
+    },
+    {
+      url: 'https://www.hengelershof.com/de/',
+      lastModified: new Date(),
+      priority: 1.0,
+      changeFrequency: 'weekly',
+    },
+    {
+      url: 'https://www.hengelershof.com/en/privacypolicy',
+      lastModified: new Date(),
+      priority: 0.5,
+      changeFrequency: 'weekly',
+    },
+    {
+      url: 'https://www.hengelershof.com/uk/privacypolicy',
+      lastModified: new Date(),
+      priority: 0.5,
+      changeFrequency: 'weekly',
+    },
+    {
+      url: 'https://www.hengelershof.com/de/privacypolicy',
+      lastModified: new Date(),
+      priority: 0.5,
+      changeFrequency: 'weekly',
+    },
+    {
+      url: 'https://www.hengelershof.com/en/imprint',
+      lastModified: new Date(),
+      priority: 0.5,
+      changeFrequency: 'weekly',
+    },
+    {
+      url: 'https://www.hengelershof.com/uk/imprint',
+      lastModified: new Date(),
+      priority: 0.5,
+      changeFrequency: 'weekly',
+    },
+    {
+      url: 'https://www.hengelershof.com/de/imprint',
+      lastModified: new Date(),
+      priority: 0.5,
+      changeFrequency: 'weekly',
+    },
+    {
+      url: 'https://www.hengelershof.com/en/events',
+      lastModified: new Date('2025-01-01'),
+      priority: 0.5,
+      changeFrequency: 'weekly',
+    },
+    {
+      url: 'https://www.hengelershof.com/uk/events',
+      lastModified: new Date('2025-01-01'),
+      priority: 0.5,
+      changeFrequency: 'weekly',
+    },
+    {
+      url: 'https://www.hengelershof.com/de/events',
+      lastModified: new Date('2025-01-01'),
+      priority: 0.5,
+      changeFrequency: 'weekly',
+    },
+    {
+      url: 'https://www.hengelershof.com/en/booking/room1',
+      lastModified: new Date(),
+      priority: 0.8,
+      changeFrequency: 'weekly',
+    },
+    {
+      url: 'https://www.hengelershof.com/uk/booking/room1',
+      lastModified: new Date(),
+      priority: 0.8,
+      changeFrequency: 'weekly',
+    },
+    {
+      url: 'https://www.hengelershof.com/de/booking/room1',
+      lastModified: new Date(),
+      priority: 0.8,
+      changeFrequency: 'weekly',
+    },
+    {
+      url: 'https://www.hengelershof.com/en/booking/room2',
+      lastModified: new Date(),
+      priority: 0.8,
+      changeFrequency: 'weekly',
+    },
+    {
+      url: 'https://www.hengelershof.com/uk/booking/room2',
+      lastModified: new Date(),
+      priority: 0.8,
+      changeFrequency: 'weekly',
+    },
+    {
+      url: 'https://www.hengelershof.com/de/booking/room2',
+      lastModified: new Date(),
+      priority: 0.8,
+      changeFrequency: 'weekly',
+    },
+    {
+      url: 'https://www.hengelershof.com/en/booking/room3',
+      lastModified: new Date(),
+      priority: 0.8,
+      changeFrequency: 'weekly',
+    },
+    {
+      url: 'https://www.hengelershof.com/uk/booking/room3',
+      lastModified: new Date(),
+      priority: 0.8,
+      changeFrequency: 'weekly',
+    },
+    {
+      url: 'https://www.hengelershof.com/de/booking/room3',
+      lastModified: new Date(),
+      priority: 0.8,
+      changeFrequency: 'weekly',
+    },
+    {
+      url: 'https://www.hengelershof.com/en/booking/house',
+      lastModified: new Date(),
+      priority: 0.8,
+      changeFrequency: 'weekly',
+    },
+    {
+      url: 'https://www.hengelershof.com/uk/booking/house',
+      lastModified: new Date(),
+      priority: 0.8,
+      changeFrequency: 'weekly',
+    },
+    {
+      url: 'https://www.hengelershof.com/de/booking/house',
+      lastModified: new Date(),
+      priority: 0.8,
+      changeFrequency: 'weekly',
+    },
+  ];
+}
